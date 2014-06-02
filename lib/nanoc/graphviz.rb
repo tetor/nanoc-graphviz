615ac94d175104bea58c7c098654e250e4bf1f0b
tetor <akihatoetsu@gmail.com>
2014-06-02 22:02:46 +0900

    Generate the template gem by Bundler

---
+++ lib/nanoc/graphviz.rb
@@ -0,0 +1,7 @@
+require "nanoc/graphviz/version"
+
+module Nanoc
+  module Graphviz
+    # Your code goes here...
+  end
+end
---
+++ lib/nanoc/graphviz/version.rb
@@ -0,0 +1,5 @@
+module Nanoc
+  module Graphviz
+    VERSION = "0.0.1"
+  end
+end
